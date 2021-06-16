423ccbd6fc18a01031b8cde0193faa7046c55610
Fernando Espana <f.espana.cordoba@gmail.com>
2021-06-16 18:13:17 -0500

    First server performance test

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const port = 8000;
 
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Hola Mundo!!!'});
+})
+
 app.listen(port, ()=> {
   console.log(`Server is running on port ${port}`);
 })
